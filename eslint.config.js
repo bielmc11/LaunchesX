6f47a78d1c3e11610ce6b210451a70b375caf5de
bielmc11 <bielmarticr@gmail.com>
2025-02-24 11:26:31 +0100

    eslint config

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslintPluginAstro from "eslint-plugin-astro";
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+    extends: "eslint:recommended",
+    overrides: [
+      {
+        files: ["*.astro"],
+        parser: "astro-eslint-parser",
+      },
+    ],
+  },
+];
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,13 @@
 import { useState } from "preact/hooks";
 
-export  function Counter() {
+export function Counter() {
     const [count, setCount] = useState(0);
 
     return (
         <div className='flex flex-col text-white gap-2 mb-6'>
             <span class='text-xl'>Count: {count}</span>
-            <button className='bg-gray-700 text-lg rounded opacity-70 cursor-pointer' onClick={() => {setCount(counter => counter + 1)}}>Increment</button>
-            <button className='bg-gray-700 text-lg rounded opacity-70 cursor-pointer' onClick={() => {setCount(counter => counter - 1)}}>decrement</button>
+            <button className='bg-gray-700 text-lg rounded opacity-70 cursor-pointer' onClick={() => { setCount(counter => counter + 1) }}>Increment</button>
+            <button className='bg-gray-700 text-lg rounded opacity-70 cursor-pointer' onClick={() => { setCount(counter => counter - 1) }}>decrement</button>
         </div>
     )
 }
