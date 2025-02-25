6b0518177265ca91b6bb5f8c4772d220d68258c2
bielmc11 <bielmarticr@gmail.com>
2025-02-25 18:45:46 +0100

    fixing bugs in production

---
+++ src/components/ChipButton.jsx
@@ -0,0 +1,6 @@
+export const ChipButton = () => {
+    console.log('bnuenas')
+    return(
+        <button className='bg-gray-700 text-lg rounded opacity-70 cursor-pointer'>Mi boton</button>
+    )
+}
