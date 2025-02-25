8a4ab3dfb83abaeab1f0863476ed9a4b5b5a33a9
bielmc11 <bielmarticr@gmail.com>
2025-02-25 13:04:27 +0100

    upcoming page + launch details

--- src/services/spacex.ts
+++ src/services/spacex.ts
@@ -1,28 +1,38 @@
-import type { Launch,LaunchesQuery } from "../types"
+import type { Launch, LaunchesQuery } from "../types";
 
 export const getLaunches = async () => {
-    const res = await fetch('https://api.spacexdata.com/v5/launches/query', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: {},
-            options: {
-                sort: {data_unix: 'desc'},
-                limit: 12
-            }
-        })
-    
-    })
-    
-    const data = await res.json() as LaunchesQuery
-    return data.docs
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: { data_unix: "desc" },
+        limit: 12,
+      },
+    }),
+  });
 
-}
+  const data = (await res.json()) as LaunchesQuery;
+  return data.docs;
+};
+
+export const getUpcomingLaunches = async () => {
+  const res = await fetch("https://api.spacexdata.com/v5/launches/upcoming", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    
+  });
+  const data = await res.json();
+  return data;
+};
 
 export const getLaunchesById = async (id: string) => {
-    const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
-    const data = await res.json() as Launch
-    return data
-}
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+  const data = (await res.json()) as Launch;
+  return data;
+};
